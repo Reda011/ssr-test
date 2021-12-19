ec4bbf67b064958da9dbfa2fa1a4004eae3ed839
taohongda <taohongda@qiyi.com>
2021-12-19 18:47:47 +0800

    客户端激活已实现，数据请求还未实现

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
         <h2>这里是 Home 页</h2>
+        <!-- 客户端激活测试 -->
+        <!-- <p>{{name}}</p>
+        <button @click="changeName">切换姓名</button> -->
     </div>
 </template>
 
@@ -10,9 +13,18 @@ export default {
     components: {},
     data () {
         return {
+            name: '李白'
         }
     },
     mounted () {},
-    methods: {}
+    methods: {
+        changeName() {
+            if(this.name == '杜甫') {
+                this.name = '李白'
+            } else {
+                this.name = '杜甫'
+            }   
+        }
+    }
 }
 </script>
